95e7bbe52b84f48022455010422569ba1004001a
marianarui <mariana.4ruiz8@gmail.com>
2023-08-04 21:01:30 -0600

    Se cambió toda la estructura del portafolio poniéndolo en pánias separadas almacenadas en directorio pages, se agregó cuestionario de contacto con validaciones, se agregaron los headers y footers con JS y se agregaron animaciones en footer, contacto e index.

---
+++ js/container1.js
@@ -0,0 +1,78 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML =
+            `
+            <!--FOOTER-->
+            <div class="container-fluid">
+                <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                    <div class="col-md-4 d-flex align-items-center">
+                        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                class="bi bi-hearts hearts1 lg" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M4.931.481c1.627-1.671 5.692 1.254 0 5.015-5.692-3.76-1.626-6.686 0-5.015Zm6.84 1.794c1.084-1.114 3.795.836 0 3.343-3.795-2.507-1.084-4.457 0-3.343ZM7.84 7.642c2.71-2.786 9.486 2.09 0 8.358-9.487-6.268-2.71-11.144 0-8.358Z" />
+                            </svg>
+                        </a>
+                        <span class="mb-3 mb-md-0 text-body-secondary" style="color: rgb(53, 79, 82);"> Elena Romero, 2023.</span>
+                    </div>
+
+                    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                        <li class="ms-3"><a class="text-body-secondary" href="https://github.com/elenallaro">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-github lg" style="color: rgb(53, 79, 82);" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                            </svg></a></li>
+                        <li class="ms-3"><a class="text-body-secondary" href="https://www.linkedin.com/in/elenallaro/">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-linkedin lg" style="color: rgb(53, 79, 82);" viewBox="0 0 16 16">
+                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                            </svg>
+                        </a></li>
+                        <li class="ms-3"><a class="text-body-secondary" href="./src/cv/CV Programadora.pdf">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-clipboard2-heart-fill lg" style="color: rgb(53, 79, 82);" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10.058.501a.501.501 0 0 0-.5-.501h-2.98c-.276 0-.5.225-.5.501A.499.499 0 0 1 5.582 1a.497.497 0 0 0-.497.497V2a.5.5 0 0 0 .5.5h4.968a.5.5 0 0 0 .5-.5v-.503A.497.497 0 0 0 10.555 1a.499.499 0 0 1-.497-.499Z"/>
+                                <path fill-rule="evenodd" d="M4.174 1h-.57a1.5 1.5 0 0 0-1.5 1.5v12a1.5 1.5 0 0 0 1.5 1.5h9a1.5 1.5 0 0 0 1.5-1.5v-12a1.5 1.5 0 0 0-1.5-1.5h-.642c.055.156.085.325.085.5V2c0 .828-.668 1.5-1.492 1.5H5.581A1.496 1.496 0 0 1 4.09 2v-.5c0-.175.03-.344.085-.5Zm3.894 5.482c1.656-1.673 5.795 1.254 0 5.018-5.795-3.764-1.656-6.69 0-5.018Z"/>
+                            </svg>
+                        </a></li>
+                    </ul>
+                </footer>
+            </div>
+            <!--Termina footer-->
+            `
+    }
+}
+class NavBar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML =
+            `
+            <div class="container-fluid">
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                <a href="/"
+                    class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32">
+                        <use xlink:href="#bootstrap" />
+                    </svg>
+                    <span class="fs-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-hearts txcl2" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4.931.481c1.627-1.671 5.692 1.254 0 5.015-5.692-3.76-1.626-6.686 0-5.015Zm6.84 1.794c1.084-1.114 3.795.836 0 3.343-3.795-2.507-1.084-4.457 0-3.343ZM7.84 7.642c2.71-2.786 9.486 2.09 0 8.358-9.487-6.268-2.71-11.144 0-8.358Z"/>
+                        </svg>
+                    </span>
+                </a>
+
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a href="../index.html" class="nav-link navtxt">Inicio</a></li>
+                    <li class="nav-item"><a href="../pages/about.html" class="nav-link navtxt">Sobre mí</a></li>
+                    <li class="nav-item"><a href="../pages/prog.html" class="nav-link navtxt">Programadora</a></li>
+                    <li class="nav-item"><a href="../pages/biol.html" class="nav-link navtxt">Bióloga</a></li>
+                    <li class="nav-item"><a href="../pages/contact.html" class="nav-link navtxt">Contacto</a></li>
+                </ul>
+            </header>
+        </div>
+                <!-- Termina Navbar -->
+            `
+    }
+}
+
+customElements.define('app-footer', Footer);
+customElements.define('app-navbar', NavBar);
---
+++ js/validations.js
@@ -0,0 +1,241 @@
+let txtNombre = document.getElementById("txtNombre");
+let txtEmail = document.getElementById("txtEmail");
+let txtPhone = document.getElementById("txtPhone");
+let btnEnviar = document.getElementById("btnEnviar");
+let txtMensaje = document.getElementById("txtMensaje");
+
+
+
+let formContacto = document.getElementById("formContacto");
+
+
+let alertNombre = document.getElementById("alertNombre");
+let alertEmail = document.getElementById("alertEmail");
+let alertPhone = document.getElementById("alertPhone");
+let alertMensaje = document.getElementById("alertMensaje");
+
+
+
+//Alertas
+let alertValidacionesTextoNombre = document.getElementById("alertValidacionesTextoNombre");
+let alertValidacionesTextoEmail = document.getElementById("alertValidacionesTextoEmail");
+let alertValidacionesTextoPhone = document.getElementById("alertValidacionesTextoPhone");
+let alertValidacionesTextoMensaje = document.getElementById("alertValidacionesTextoMensaje");
+
+let index = [];
+
+function validarNombre(nombre) {
+    if (nombre.length >= 3 && nombre.length < 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Regex Email
+let regexEmail =
+    /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+function validarEmail(email) {
+    if (email != "") {
+        if (regexEmail.test(email)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function validarMensaje(mensaje) {
+    if (mensaje.length >= 20 && mensaje.length <= 200) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let regextel = /^(\(\+?\d{2,3}\)[\*|\s|\-|\.]?(([\d][\*|\s|\-|\.]?){6})(([\d][\s|\-|\.]?){2})?|(\+?[\d][\s|\-|\.]?){8}(([\d][\s|\-|\.]?){2}(([\d][\s|\-|\.]?){2})?)?)$/;
+function validarNumTel(numTel) {
+    if (numTel != "") {
+        if (regextel.test(numTel)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function recibirInfoIsChecked() {
+    if (checkrecibirInfo.checked) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+btnEnviar.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (!validarNombre(txtNombre.value)) {
+        if (!index.includes("nombre")) {
+            alertValidacionesTextoNombre.insertAdjacentHTML(
+                "afterbegin", ` El <strong> Nombre </strong> no es correcto. <br/> `);
+            alertValidacionesTextoNombre.style.color = "red";
+            txtNombre.style.border = "solid thin red";
+            isValid = false;
+            index.push("nombre");
+        }
+    }
+    if (!validarEmail(txtEmail.value)) {
+        if (!index.includes("email")) {
+            alertValidacionesTextoEmail.insertAdjacentHTML("afterbegin", `El <strong> Correo </strong> no es correcto. <br/> `);
+            alertValidacionesTextoEmail.style.color = "red";
+            txtEmail.style.border = "solid thin red";
+            isValid = false;
+            index.push("email");
+        }
+
+    }
+
+    if (!validarNumTel(txtPhone.value)) {
+        if (!index.includes("phone")) {
+            alertValidacionesTextoPhone.insertAdjacentHTML("afterbegin", `El <strong> Número telefónico </strong> no es correcto. <br/> `);
+            alertValidacionesTextoPhone.style.color = "red";
+            txtPhone.style.border = "solid thin red";
+            isValid = false;
+            index.push("phone");
+        }
+
+    }
+
+    if (!validarMensaje(txtMensaje.value)) {
+        if (!index.includes("mensaje")) {
+            alertValidacionesTextoMensaje.insertAdjacentHTML("afterbegin", `El <strong> Mensaje</strong> no es correcto. <br/> `);
+            alertValidacionesTextoMensaje.style.color = "red";
+            txtMensaje.style.border = "solid thin red";
+            isValid = false;
+            index.push("mensaje");
+        }
+    }
+
+    if (validarEmail(txtEmail.value) && validarEmail(txtEmail.value) && validarNumTel(txtPhone.value)) {
+        enviarEmail();
+        index = [];
+    }
+});
+
+
+
+function enviarEmail() {
+    const serviceID = 'service_9r31h4b';
+    const templateID = 'template_o4c2eck';
+    emailjs.sendForm(serviceID, templateID, formContacto)
+        .then(() => {
+            alertValidacionesEnviar.insertAdjacentHTML("afterbegin", `<strong>Gracias por comunicarte con nosotros.</strong> <br/> `);
+            alertValidacionesEnviar.style.display = "block";
+            alertValidacionesEnviar.style.color = "green";
+            alertEnviar.style.border = "solid thin green";
+        }, (err) => {
+            alert(JSON.stringify(err));
+        });
+}
+
+
+
+//Listener para validar el nombre cada vez que el usuario teclee algo en el campo nombre
+txtNombre.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (!validarNombre(txtNombre.value)) {
+        if (!index.includes("nombre")) {
+            alertValidacionesTextoNombre.insertAdjacentHTML(
+                "afterbegin", ` El <strong> Nombre </strong> no es correcto. <br/> `);
+            alertValidacionesTextoNombre.style.color = "red";
+            txtNombre.style.border = "solid thin red";
+            isValid = false;
+            index.push("nombre");
+        }
+    }//if nombre no cumple las validaciones
+    else {
+        //quitar alertas
+        alertValidacionesTextoNombre.innerHTML = "";
+        alertNombre.style.display = "none";
+        txtNombre.style.border = "";
+        removeAllInstances(index, "nombre");
+    }
+
+});
+
+
+txtEmail.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (!validarEmail(txtEmail.value)) {
+        if (!index.includes("email")) {
+            alertValidacionesTextoEmail.insertAdjacentHTML("afterbegin", `El <strong> Correo </strong> no es correcto. <br/> `);
+            alertValidacionesTextoEmail.style.color = "red";
+            txtEmail.style.border = "solid thin red";
+            isValid = false;
+            index.push("email");
+        }
+    }//if email no cumple las validaciones
+    else {
+        //quitar alertas
+        alertValidacionesTextoEmail.innerHTML = "";
+        alertEmail.style.display = "none";
+        txtEmail.style.border = "";
+        removeAllInstances(index, "email");
+    }
+
+});
+
+txtPhone.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (!validarNumTel(txtPhone.value)) {
+        if (!index.includes("phone")) {
+            alertValidacionesTextoPhone.insertAdjacentHTML("afterbegin", `El <strong> Número telefónico </strong> no es correcto. <br/> `);
+            alertValidacionesTextoPhone.style.color = "red";
+            txtPhone.style.border = "solid thin red";
+            isValid = false;
+            index.push("phone");
+        }
+
+    }//if phone no cumple las validaciones 
+    else {
+        //quitar alertas
+        alertValidacionesTextoPhone.innerHTML = "";
+        alertPhone.style.display = "none";
+        txtPhone.style.border = "";
+        removeAllInstances(index, "phone");
+    }
+
+});
+
+txtMensaje.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (!validarMensaje(txtMensaje.value)) {
+        if (!index.includes("mensaje")) {
+            alertValidacionesTextoMensaje.insertAdjacentHTML("afterbegin", `El <strong> Mensaje</strong> no es correcto. <br/> `);
+            alertValidacionesTextoMensaje.style.color = "red";
+            txtMensaje.style.border = "solid thin red";
+            isValid = false;
+            index.push("mensaje");
+        }
+    }//if mensaje no cumple las validaciones 
+    else {
+        //quitar alertas
+        alertValidacionesTextoMensaje.innerHTML = "";
+        alertMensaje.style.display = "none";
+        txtMensaje.style.border = "";
+        removeAllInstances(index, "mensaje");
+    }
+});
+
+
+
+//Remueve todas las instancias de un objeto dado (item) que se encuentre en el arreglo index
+function removeAllInstances(arr, item) {
+    for (var i = arr.length; i--;) {
+        if (arr[i] === item) arr.splice(i, 1);
+    }
+}
